a05365a366d62fa63428fd3880c048757905fb14
Jun Bae <43739199+applecider327@users.noreply.github.com>
2020-08-27 21:29:58 +0700

    Update by_descriptions.py

--- by_descriptions.py
+++ by_descriptions.py
@@ -1,4 +1,4 @@
-# As mentioned in the README file sometimes it's the descriptions that come into play. 
+# As mentioned in the README file sometimes it's the descriptions that come into play. Libraries required: nltk, re, and pandas.
 
 # Helper Function 1
 # what it does: identify the heart defect type in question given a sentence of its description. takes use of description_patterns (refer to description_patterns.py)
@@ -10,3 +10,26 @@ def describes_what(sent):
         return None
     return search_id_k(ids[n[0]])[0]
     
+# Example 1
+# describes_what("We introduce a patient with the following four conditions: ventricular septal defect, right ventricular hypertrophy, overriding aorta, and pulmonary stenosis.")
+
+# "Tetralogy of Fallot"
+
+# Example 2
+# describes_what("Her left coronary artery is connected to the pulmonary artery instead of the aorta.")
+
+# "Anomalous Left Coronary Artery"
+
+# Main Function
+# what it does: basically an extension of describes_what; takes in a paragraph and looks at every sentence for the right description patterns and returns all match
+
+def by_description(text):
+    series = pd.Series(nltk.sent_tokenize(text.lower()))
+    return series.apply(describes_what).value_counts()
+
+# Example 1
+# by_description("We introduce a newborn with duplication of one of the chromosome 18s. There also seems to be a hole in the septum that usually separates the two lower chambers of a heart.")
+
+# Trisomy 18 Syndrome                  1
+# Heart Septal Defects, Ventricular    1
+# dtype: int64
