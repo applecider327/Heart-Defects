fc2ed9003c9f9036babf968aa74b02820b61683e
Jun Bae <43739199+applecider327@users.noreply.github.com>
2021-08-17 15:25:05 +0700

    Update by_description.py

--- by_description.py
+++ by_description.py
@@ -2,7 +2,7 @@
 
 # Helper Function 1
 # what it does: identify the heart defect type in question given a sentence of its description. takes use of description_patterns (refer to description_patterns.py)
-# and search_id_k from fetching_terms_and_descriptions. 
+# and search_id_k from extracting_data. 
 
 def describes_what(sent):
     n = [description_patterns.index(each) for each in description_patterns if len(re.findall(str(each[0]), sent)) != 0]
